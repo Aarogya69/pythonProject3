272cb49aa77e082d7cd8f86f7b0d47ea8cb1711f
Aaron <aarogyathapaliya123@gmail.com>
2021-02-23 10:00:12 +0545

    stu=int(input("Enter number of students")) app=int(input("Enter number of apples")) appleperstu= stu / app remapple = app - appleperstu print('Each students get',{appleperstu}) print('The remaining students are',{remapple})

---
+++ Area.py
@@ -0,0 +1,4 @@
+base=float(input('Enter a number'))
+height=float(input('Enter a number'))
+rtangle=1/2 * base * height
+print(rtangle)
---
+++ Basket.py
@@ -0,0 +1,6 @@
+stu=int(input("Enter number of students"))
+app=int(input("Enter number of apples"))
+appleperstu= stu / app
+remapple = app - appleperstu
+print('Each students get',{appleperstu})
+print('The remaining students are',{remapple})
