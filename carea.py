d2e6d63e8b06f01b1271a599ae84907b588c670a
Aaron <aarogyathapaliya123@gmail.com>
2021-03-16 09:46:09 +0545

    initial commit

---
+++ carea.py
@@ -0,0 +1,4 @@
+r=int(input("Enter a radius")
+pi = 3.14
+area = pi * r * r
+print(f"The area of circle is{area}")
---
+++ listadd.py
@@ -0,0 +1,3 @@
+l=[1,3,5,6,7,8,9]
+total=sum(l)
+print(f"The sum of the total number in list is {total}")
