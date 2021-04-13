c1343c32369c86ca4fcaa4733973977ad0c3ecc9
Aaron <aarogyathapaliya123@gmail.com>
2021-04-13 09:46:12 +0545

    simple cal

---
+++ no3.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter the first number"))
+
+
+if num1%2:
+    print("It is Odd number")
+
+else:
+    print("It is Even  number")
---
+++ w8.py
@@ -0,0 +1,18 @@
+weight=int(input("Enter the weight of a person"))
+if weight == "kg":
+   kg= weight * 2.20
+    print(f"Kg into lbs is {kg}")
+elif weight=="lbs":
+    lbs= weight /2.20
+    print(f"Lbs into kg {lbs}")
+else:
+    print("Its is wrong")
+
+
+
+
+
+
+
+
+    game finding secreat number within 3 atttemots using while loops
