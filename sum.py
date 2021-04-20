ae565916e3b0d86fe6faa28910910e1f8f1c3026
Aaron <aarogyathapaliya123@gmail.com>
2021-04-20 09:57:45 +0545

    new work

---
+++ sum.py
@@ -0,0 +1,8 @@
+a=int(input("Enter first integer"))
+b=int(input("Enter second integer"))
+c=int(input("Enter third number"))
+
+if a==b & a==c:
+    print("The sum is zero")
+else:
+    print("The sun is not zero")
